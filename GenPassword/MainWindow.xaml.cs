88cd0523d4e7d174d9a6223ff82787317c0450c7
Istkliff <garnakot191@gmail.com>
2020-01-04 17:43:07 +0300

    Main function

--- GenPassword/MainWindow.xaml.cs
+++ GenPassword/MainWindow.xaml.cs
@@ -20,9 +20,23 @@ namespace GenPassword
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		Random rnd = new Random(); //Random method
+		string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz/*-+.=#@!%$^&()?|\"'/"; //Main symbols for random generation
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void GenPass(object sender, RoutedEventArgs e) //Random generator method
+		{
+			string result = ""; //Reset the password string
+
+			for (int i = rnd.Next(5,50); i > 0; i--)
+			{
+				result += chars[rnd.Next(chars.Length)]; //Generation password with random size
+			}
+
+			FinalPass.Text = result; //Broadcast to textbox
 		}
 	}
 }
